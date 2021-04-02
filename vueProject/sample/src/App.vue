6bb5f6376e65484e62d78ec00ae10be62b0cb53f
jny <ny030303@naver.com>
2021-04-02 15:39:42 +0900

    add sample 0402

---
+++ vueProject/sample/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <A02Binding/>
+  </div>
+</template>
+
+<script>
+import A02Binding from './components/02Binding.vue'
+
+export default {
+  name: 'App',
+  components: {
+    A02Binding
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueProject/sample/src/components/02Binding.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="card-body">
+        <h3>{{title}}</h3>
+        <br>
+
+        <div>
+            v-text: <span v-text="text"></span><br>
+            v-html: <span v-html="text"></span><br>
+            v-once: <span v-once>{{text}}</span><br>
+            v-pre: <span></span><br>
+            <br>
+            <button>Change</button>
+            <button>Change</button>
+            <br>
+            <br>
+            
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <br>
+            
+            <input type="checkbox" value="apple">사과, 
+            <input type="checkbox" value="banana">바나나, 
+            <input type="checkbox" value="melon">멜론<br>
+            
+            <div></div>
+            <div><span></span></div>
+        <br>
+
+        computed: <br>
+        <br>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        title: "2.Binding",
+        text: '<b>asdf</b>'
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
